1fd2e0381278cd7176d0dc8cf7216da0c970d4e0
Tianyu Justin Yang <tyjyang@mit.edu>
2023-04-18 02:33:21 +0200

    PU cleanup report; clearnup script improvements

--- MSPileupCleanup.py
+++ MSPileupCleanup.py
@@ -1,14 +1,29 @@
 import json
 import os
+from collections import OrderedDict
 
 f = open("../WmAgentScripts/campaigns.json", "r")
-campaigns = json.loads(f.read())
+campaigns = json.loads(f.read(), object_pairs_hook=OrderedDict)
 
 i = 0
 pus = []
+pus_to_clean = []
 for campaign, config in campaigns.items():
     if "secondaries" in config.keys():
         for pu in config["secondaries"].keys():
-            rucio_out = os.popen(f"rucio list-dataset-replicas cms:{pu}")
-            print(rucio_out)
+            pus.append(pu)
+            rucio_out = os.popen("rucio list-dataset-replicas cms:" + pu).read()
+            if "DATASET" not in rucio_out:
+                pus_to_clean.append(pu)
+                config["secondaries"].pop(pu, None)
+campaigns_str = json.dumps(campaigns, indent = 4)
+
+with open("PU-clearn-report.txt", "w") as outfile:
+    outfile.write("The list of PUs found:\n")
+    for pu in pus: outfile.write(pu+"\n")
+    outfile.write("The list of PUs removed:\n")
+    for pu in pus_to_clean: outfile.write(pu+"\n")
+
+with open("campaigns-new.json", "w") as outfile:
+    outfile.write(campaigns_str)
 
