ddbbdbbe6bd2472f828a93cc737aab95b16eff4a
Tianyu Justin Yang <tyjyang@mit.edu>
2023-04-18 16:51:24 +0200

    updated log

--- MSPileupCleanup.py
+++ MSPileupCleanup.py
@@ -5,25 +5,53 @@ from collections import OrderedDict
 f = open("../WmAgentScripts/campaigns.json", "r")
 campaigns = json.loads(f.read(), object_pairs_hook=OrderedDict)
 
-i = 0
-pus = []
-pus_to_clean = []
-for campaign, config in campaigns.items():
-    if "secondaries" in config.keys():
-        for pu in config["secondaries"].keys():
-            pus.append(pu)
-            rucio_out = os.popen("rucio list-dataset-replicas cms:" + pu).read()
-            if "DATASET" not in rucio_out:
-                pus_to_clean.append(pu)
-                campaigns[campaign]["secondaries"].pop(pu, None)
-campaigns_str = json.dumps(campaigns, indent = 2, separators=(',', ': '))
+# get the list of sites on which the dataset is protected by a rule undre a particular rucio account 
+def get_sites_with_account_rules(dataset, rucio_account):
+    outstr = os.popen("rucio list-rules cms:" + dataset).read()
+    rules = [x for x in outstr.split('\n') if ("ID" not in x and "-----" not in x and x != "")] 
+    res = []
+    for rule in rules:
+        rule_params = rule.split()
+        rule_account, rule_site = rule_params[1], rule_params[4]
+        if rule_account == rucio_account:
+            if "|" in rule_site:
+                for x in rule_site.split("|"):
+                    res.append(x)
+            res.append(rule_site)
+    return res
 
-with open("PU-clearn-report.txt", "w") as outfile:
-    outfile.write("The list of PUs found:\n")
-    for pu in pus: outfile.write(pu+"\n")
-    outfile.write("The list of PUs removed:\n")
-    for pu in pus_to_clean: outfile.write(pu+"\n")
+with open("log_cleanup.txt", "w") as log:
+    i = 0
+    pus = []
+    pus_to_clean = []
+    for campaign, config in campaigns.items():
+        if "secondaries" in config.keys():
+            for pu in config["secondaries"].keys():
+                if pu not in pus: pus.append(pu)
+                pu_sites_wmcore = get_sites_with_account_rules(pu, "wmcore_transferor")
+                if len(pu_sites_wmcore) == 0:
+                    campaigns[campaign]["secondaries"].pop(pu)
+                    i += 1
+                    log.write(str(i) + ". ++++++++++++++++++++++++++++++++++++++++++++++\n")
+                    log.write("removed PU " + pu + " in campaign " + campaign + " due to no rules under wmcore_transferor.")
+                    log.write("\n")
+                    continue
+                if "SiteWhitelist" in config["secondaries"][pu]: pu_sites_config = config["secondaries"][pu]["SiteWhitelist"]
+                else: pu_sites_config = []
+                if any([x not in pu_sites_wmcore for x in pu_sites_config]):
+                    config["secondaries"][pu]["SiteWhitelist"] = pu_sites_wmcore
+                    i += 1
+                    log.write(str(i) + ". ++++++++++++++++++++++++++++++++++++++++++++++\n")
+                    log.write("changed site whitelist for " + pu + " from:\n")
+                    for x in pu_sites_config: log.write(x + ' ')
+                    log.write("\n")
+                    log.write("to:\n")
+                    for x in pu_sites_wmcore: log.write(x + ' ')
+                    log.write("\n")
 
+    campaigns_str = json.dumps(campaigns, indent = 2, separators=(',', ': '))
+
+    
 with open("campaigns-new.json", "w") as outfile:
     outfile.write(campaigns_str)
 
