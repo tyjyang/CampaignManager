9e93a787bf4f66aa6a9799c396d7e3cf4f1099b2
Tianyu Justin Yang <tyjyang@mit.edu>
2023-06-13 18:00:15 +0200

    script for syncing campaigns to couchDB

---
+++ scripts/sync-couchDB.py
@@ -0,0 +1,19 @@
+import argparse
+import io_tools
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--campConfig", type = str, help = "json file for campaign configuration")
+parser.add_argument("-a", "--campsToAdd", type = str, nargs = "*", help = "new campaigns added to the config")
+parser.add_argument("--dirWmAgentScripts", type = str, default = "/data/unifiedPy3-fast/WmAgentScripts", help = "path to the WmAgentScripts repo")
+
+args = parser.parse_args()
+
+campaigns_config = io_tools.import_jsonfile_as_OrderedDict(args.campConfig)
+
+os.system(f"python3 {args.dirWmAgentScripts}/campaignsConfiguration.py --load {args.campConfig}")
+
+for camp in args.campsToAdd:
+    camp_config = dict(campaigns_config[camp])
+    os.system(f"python3 {args.dirWmAgentScripts}/campaignsConfiguration.py --name {camp} --configuration {camp_config}")
+
